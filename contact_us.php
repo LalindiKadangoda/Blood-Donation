2b3c51b0f7274df9f9225866e524ce8225209ed9
LalindiKadangoda <lalikandangodag4@gmail.com>
2025-07-20 18:14:34 +0530

    First commit

---
+++ contact_us.php
@@ -0,0 +1,62 @@
+<?php
+include("config.php");
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contact Us </title> 
+<link rel = "stylesheet" href = "styles/contact_us.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+</head>
+<body>
+
+    <header class="header">
+        <?php
+            include "header.php";
+        ?>
+    </header>
+
+    <div class="contdiv">
+        <div class="wrapper">
+            <h1 style ="color :red"><b> Contact Us</h1></b>
+        <div>
+        <br>
+
+    <h2 class ="para "style = "background-color: #00DDFF";>
+    <p ><center> Do not hesitate to get in touch, our team is</center> <div><center> 24/7/365 at your disposal!</center>
+    </p></h2>
+    <br>
+    <h2 class ="contacts"> <ul>
+    <li> TELEPHONE : +94113269830/+94762459320</li>
+    <br>
+    <li> FAX       : 212-693-2377</li>
+    <br>
+    <li> Email     : quickdonororg@gmail.com </li>
+    </ul>
+    </h2>
+    <br>
+    <br>
+    <h2> Also we are on ; </h2><br>
+    <div class="media">
+            <a href="https://www.facebook.com/"><img src="images/upeksha/facebook.png" alt="Facebook"></a>
+		    <a href="https://www.instagram.com/"><img src="images/upeksha/insta.png" alt="instergram"></a>
+		    <a href="https://www.linkedin.com"><img src="images/upeksha/linkedln.png" alt="linkedln"></a>
+			<a href="https://twitter.com/"><img src="images/upeksha/twitter.png" alt="twitter"></a>
+		   
+			 </div>
+                
+        </div></div></div>
+
+    <footer class="footer">
+        <?php
+            include "footer.php";
+        ?>
+    </footer>
+		
+</body>
+</html>
+
+			 
